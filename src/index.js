9a766157c58658f939b1c1558eeac629211cc706
RomanTrubin <trubin-roma@yandex.ru>
2023-08-10 23:03:20 +0400

    feat: done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+const { sort } = require("semver");
+
+module.exports = function towelSort(matrix) {
+  if (!matrix) {
+    return []
+  }
+  let result = [];
+  for (let i = 0; i < matrix.length; i++) {
+    if (i % 2 !== 0) { result.push((matrix[i]).reverse()) } else { result.push(matrix[i]) }
+  }
+  // console.log(result.flat(Infinity))
+  return result.flat(Infinity);
 }
+
+// towelSort([
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9],
+// ])
